382049e1814d2458b798b1b89a7f820a0e7e484d
TommasoAgo <74262446+TommasoAgo@users.noreply.github.com>
2021-05-10 14:26:28 +0200

    Fine Header

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,13 @@
   \********************/
 /***/ (() => {
 
-
+var app = new Vue({
+  el: '#root',
+  data: {
+    // Header content strings Array
+    header_menu: ['Home', 'Services', 'About', 'Videos', 'Blog', 'Store']
+  }
+});
 
 /***/ }),
 
---
+++ src/app.js
@@ -0,0 +1,17 @@
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+
+            // Header content strings Array
+            header_menu: [
+                'Home',
+                'Services',
+                'About',
+                'Videos',
+                'Blog',
+                'Store'
+            ]
+        }
+    }
+)
